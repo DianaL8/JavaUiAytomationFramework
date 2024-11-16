376e0be50a7565ea9f2765c8252183e4c509ec62
DianaL8 <lozovanu.diana8@gmail.com>
2024-11-16 16:40:47 +0200

    Implement the Driver Manager and configure the Selenium dependencies.

--- src/main/java/online/tekwill/TestRunner.java
+++ src/main/java/online/tekwill/TestRunner.java
@@ -1,7 +1,30 @@
 package online.tekwill;
 
+import online.tekwill.managers.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+
 public class TestRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // System.out.println("Hello world!");
+
+        //DriverManager.getInstance().getDriver().get("https://www.google.com/");
+        WebDriver driver = DriverManager.getInstance().getDriver();
+        driver.get("https://www.google.com/");
+
+        String theNameOfTheFirstTab = driver.getWindowHandle();
+
+        // Open a new window and navigate to diez page
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://diez.md/");
+        Thread.sleep("5000");
+        // Close the current tab
+        driver.close();
+
+        driver.switchTo().window(theNameOfTheFirstTab);
+        driver.get("https://www.stiri.md");
+        Thread.sleep("3000");
+        driver.quit();
     }
 }
+//DriverManager.getInstance().getDriver().quit();
